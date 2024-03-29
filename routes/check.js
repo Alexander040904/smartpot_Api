ad945771fa2cfc46b7f58c06a4b906c45f9d2632
Alexander040904 <jonathanalexanderramirezortega@gmail.com>
2024-03-28 20:41:14 -0600

    example completo

--- routes/check.js
+++ routes/check.js
@@ -1,47 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const app = express();
-app.use(cors()); // Esto permitirá todas las solicitudes CORS
-const url = 'mongodb://localhost:27017';
+app.use(cors()); 
 
-// Opciones de conexión
+const url = 'mongodb://localhost:27017';
 const options = {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// Función para conectarse a la base de datos y configurar el observador de cambios
+async function configureReplicaSet() {
+  const client = new MongoClient(url, options);
+
+  try {
+    await client.connect();
+    console.log('Conexión exitosa a la base de datos para configurar el conjunto de réplicas');
+
+    const adminDb = client.db('admin');
+    const result = await adminDb.command({ replSetInitiate: { _id: 'RS', members: [{ _id: 0, host: 'localhost:27017' }] } });
+
+    console.log('Configuración del conjunto de réplicas completada:', result);
+  } catch (error) {
+    console.error('Error al configurar el conjunto de réplicas:', error);
+  } finally {
+    await client.close();
+  }
+}
+
 async function watchDatabase() {
   const client = new MongoClient(url, options);
 
   try {
-    // Conexión a la base de datos
     await client.connect();
+    console.log('Conexión exitosa a la base de datos');
 
-    // Base de datos y colección que deseas observar
     const db = client.db('smarpot');
     const collection = db.collection('usuarios');
 
-    // Configurar el observador de cambios
     const changeStream = collection.watch();
-    
 
-    // Escuchar los eventos de cambio
- 
+    changeStream.on('change', (change) => {
+      console.log('Se detectó un cambio en la base de datos:', change);
+    });
 
+    changeStream.on('error', (error) => {
+      console.error('Error en el ChangeStream:', error);
+    });
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
   }
 }
 
-// Iniciar el observador de cambios
-watchDatabase();
+configureReplicaSet(); // Paso 4: Configurar el conjunto de réplicas
+watchDatabase(); // Iniciar el observador de cambios en la base de datos
 
-// Ruta de ejemplo que devuelve "true" al recibir una solicitud GET
 router.get('/', (req, res) => {
   res.send('true');
 });
 
 module.exports = router;
-
