f8d6345c107dc748b25a4b570fdcdab63043df02
Alexander090409 <jonathanalexanderramirezortega@gmail.com>
2024-03-28 20:58:44 -0600

    version de envio de correo 01

--- routes/check.js
+++ routes/check.js
@@ -1,31 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const app = express();
 app.use(cors()); // Esto permitirá todas las solicitudes CORS
 
 
 const url = "mongodb+srv://halcorporation40:151081halco@smarpot.iddzpk2.mongodb.net/?retryWrites=true&w=majority&appName=smarpot";
 
-// Opciones de conexión
+
 const options = {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-// Función para conectarse a la base de datos y configurar el observador de cambios
+async function configureReplicaSet() {
+  const client = new MongoClient(url, options);
+
+  try {
+    await client.connect();
+    console.log('Conexión exitosa a la base de datos para configurar el conjunto de réplicas');
+
+    const adminDb = client.db('admin');
+    const result = await adminDb.command({ replSetInitiate: { _id: 'RS', members: [{ _id: 0, host: 'localhost:27017' }] } });
+
+    console.log('Configuración del conjunto de réplicas completada:', result);
+  } catch (error) {
+    console.error('Error al configurar el conjunto de réplicas:', error);
+  } finally {
+    await client.close();
+  }
+}
+
 async function watchDatabase() {
   const client = new MongoClient(url, options);
 
   try {
-    // Conexión a la base de datos
     await client.connect();
+    console.log('Conexión exitosa a la base de datos');
 
-    // Base de datos y colección que deseas observar
     const db = client.db('smarpot');
     const collection = db.collection('usuarios');
 
-    // Configurar el observador de cambios
     const changeStream = collection.watch();
     changeStream.on('change', (change) => {
 
@@ -35,21 +51,18 @@ async function watchDatabase() {
       // Aquí puedes agregar la lógica para manejar los cambios
     });
 
-    // Escuchar los eventos de cambio
- 
-
+  
+    
   } catch (error) {
     console.error('Error al conectar a la base de datos:', error);
   }
 }
 
-// Iniciar el observador de cambios
-watchDatabase();
+configureReplicaSet(); // Paso 4: Configurar el conjunto de réplicas
+watchDatabase(); // Iniciar el observador de cambios en la base de datos
 
-// Ruta de ejemplo que devuelve "true" al recibir una solicitud GET
 router.get('/', (req, res) => {
   res.send('true');
 });
 
 module.exports = router;
-
